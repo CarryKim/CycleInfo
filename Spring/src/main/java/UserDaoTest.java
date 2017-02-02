f3a3597da6908e83e92f6b181d7e82a88de9e23a
CarryKim <mskim@mskim-PC>
2017-02-02 14:45:44 +0900

    UserDaoTest 추가

---
+++ Spring/src/main/java/UserDaoTest.java
@@ -0,0 +1,17 @@
+import java.sql.SQLException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import springbook.user.dao.DaoFactory;
+import springbook.user.dao.UserDao;
+
+public class UserDaoTest {
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+		
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao", UserDao.class);
+	}
+
+}
